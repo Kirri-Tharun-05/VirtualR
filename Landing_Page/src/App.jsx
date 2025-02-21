20d81ab419975414c1c2c0561463a239fc1e0fc3
Kirri Tharun <tharunprajitha2023@gmail.com>
2025-02-21 23:24:11 +0530

    updated Feature Section

--- Landing_Page/src/App.jsx
+++ Landing_Page/src/App.jsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import Navbar from './components/Navbar'
 import Hero from './components/Hero';
+import Features from './components/Features';
 function App() {
   return (
     <>
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
+        <Features />
       </div>
     </>
   );
