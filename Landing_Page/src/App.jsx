1bf3fb8a2bf18208dd8a1fd590da78f9d3780a19
Kirri Tharun <tharunprajitha2023@gmail.com>
2025-02-22 09:45:28 +0530

    Added New Sections

--- Landing_Page/src/App.jsx
+++ Landing_Page/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from './components/Navbar'
 import Hero from './components/Hero';
 import Features from './components/Features';
+import Workflow from './components/Workflow';
+import Pricing from './components/Pricing';
 function App() {
   return (
     <>
@@ -9,6 +11,8 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
         <Features />
+        <Workflow/>
+        <Pricing/>
       </div>
     </>
   );
